6e5c5882839b0098775a2c685a5ed35e367f2d2b
giornn0 <shaaamsuu@gmail.com>
2021-09-27 18:00:58 -0300

    ready to deploy

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ console.log('SERVER DHARMA')
 startConnection()
 const numCPUs = cpus().length;
 
-// if(cluster.isPrimary){
-//   for(let i = 0; i<numCPUs; i++){
-//     cluster.fork()
-//   }
-//   cluster.on('exit',(worker,code,signal)=>{
-//     console.log(`Worker ${worker.process.pid} died`)
-//   })
-// }else{
+if(cluster.isPrimary){
+  for(let i = 0; i<numCPUs; i++){
+    cluster.fork()
+  }
+  cluster.on('exit',(worker,code,signal)=>{
+    console.log(`Worker ${worker.process.pid} died`)
+  })
+}else{
   const app = http.createServer().listen(5000, () => {
       console.log(`Server running in port 5000`);
     });
     app.on('request',onRequest)
-// }
+}
